6f17151296960cf72e9a21cbda4475a1830179a3
Théophile Choutri <theophile@choutri.eu>
2024-06-25 12:36:26 +0200

    Fix the path to the macOS arm64 pre-release

--- .github/workflows/process-binaries.sh
+++ .github/workflows/process-binaries.sh
@@ -2,6 +2,8 @@
 
 CONFER_EXEC=$(cabal list-bin --project-file=cabal.release.project confer:exe:confer)
 
+ldd $CONFER_EXEC
+
 case "$(uname -s)" in
         Linux*) strip $CONFER_EXEC && upx -9 $CONFER_EXEC;;
         Darwin*) strip $CONFER_EXEC;;
