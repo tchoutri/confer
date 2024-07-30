405d90c31dff36070dad09f80047d2d427b720d0
Théophile Choutri <theophile@choutri.eu>
2024-07-30 21:37:42 +0200

    Try something naive

--- .cirrus/ci.sh
+++ .cirrus/ci.sh
@@ -2,22 +2,38 @@
 
 set -xEeuo pipefail
 
-build_freebsd() {
+setup_freebsd() {
   export GHCUP_INSTALL_BASE_PREFIX="/opt/ghc/"
-  mkdir -p ~/.local/ghcup/bin/
+  mkdir -p $HOME/.local/ghcup/bin/
+  mkdir -p /opt/ghcup/bin/
+  export PATH=$HOME/.local/ghcup/bin:/opt/ghcup/bin:$PATH
   curl -L https://downloads.haskell.org/~ghcup/x86_64-portbld-freebsd-ghcup --output /opt/ghcup/bin/ghcup
   chmod +x /opt/ghcup/bin/ghcup
   ghcup install ghc 9.8.2
   ghcup set ghc 9.8.2
   ghcup install cabal 3.12.1.0
+  cabal update
+}
+
+configure_freebsd() {
+  cabal freeze --project-file=cabal.release.project
+}
+
+build_freebsd() {
+  cabal build --project-file=cabal.release.project all -j
+}
+
+test_freebsd() {
+  cabal test --project-file=cabal.release.project all -j
 }
 
 help() {
-  echo "Run with \`build_freebsd\`"
+  echo "Run with setup_freebsd, configure_freebsd, build_freebsd or test_freebsd."
 }
 
 case ${1:-help} in
   help) help ;;
-  build_frebsd) build_freebsd ;;
-
+  setup_freebsd) setup_freebsd ;;
+  build_freebsd) build_freebsd ;;
+  test_freebsd) test_freebsd ;;
 esac
